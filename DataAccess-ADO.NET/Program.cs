e4855908400bfb630007ccaa51219101e224680c
Luiz Araujo <arauldesign@gmail.com>
2022-07-18 11:38:05 -0300

    sqlcommand e sqldatareader

--- DataAccess-ADO.NET/Program.cs
+++ DataAccess-ADO.NET/Program.cs
@@ -18,6 +18,22 @@ namespace DataAccess_ADO.NET
             using (var connection = new SqlConnection(connectionString))
             {
                 Console.WriteLine("Conectado!");
+                connection.Open();
+
+                using(var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+                    //command.ExecuteNonQuery(); para não leitura
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+                    }
+
+                }
             }
         }
     }
